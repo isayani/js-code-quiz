c116a73e9c522b09b47110cb51d60239e26d4c9e
isayani <isayani95@gmail.com>
2022-07-31 00:09:36 -0400

    init commit html card and prelim js

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// object array for questions, multiple choice options, and corresponding answers
+var questions = [
+    
+    {question: "Commonly used data types DO NOT include:", 
+    options: ["strings", "booleans", "alerts", "numbers"], 
+    answer: "alerts"},
+
+    {question: "The condition in an 'if/else' statement is enclosed within ______.", 
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"], 
+    answer: "parentheses"},
+
+    {question: "Arrays in Javascript can be used to store ______.", 
+    options: ["numbers & strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"},
+
+    {question: "String values must be enclosed within ______ when being assigned to variables.", 
+    options: ["commas", "curly brackets", "quotes", "parantheses"], 
+    answer: "quotes"},
+
+    {question: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+    options: ["Javascript", "terminal/bash", "for loops", "console.log"], 
+    answer: "console.log"}
+];
