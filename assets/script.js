dfefa64138f98cce46b6c7e96f6c5dbd04162b66
isayani <isayani95@gmail.com>
2022-07-31 20:18:57 -0400

    jQuery working for questions

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,11 @@
 var timer = $('.timer');
 var score
-var questionEl = $('.question');
+var questionEl = $('#question');
+var parentQuestionEl = $('.question');
 var answerEl = $('.answers');
 var options = [$("<button>"), $("<button>"), $("<button>"), $("<button>")];
 var card = $('.card-body');
 var shuffledQuestions, currentQuestionIndex
-
-
-var startButton = $('.start-btn');
-$(startButton).on('click', startQuestion);
-
-// answer key format (quiz.method)
-// var quiz = {
-
-// when start button is clicked
-function startQuestion () {
-    // hide all elements in instr card
-    $(card).addClass('hide');
-    // start the clock
-    // pull the questions in and make display (might be easier to make sample question hidden on html)
-    $(questionEl).removeClass('hide');
-    // shuffle questions
-    shuffledQuestions = questions.sort(()=> Math.random() - .5);
-    currentQuestionIndex = 0;
-    setNextQuestion()
-    // start populating board (questions and answers)
-    // append to card-body div
-};
-
-function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-};
-
-function showQuestion(question) {
-    $(questionEl).text(question.question);
-    question.options.forEach(options => {
-        var button = document.createElement('button')
-        button.innerText = options.text
-        button.classList.add('btn')
-    if (options.answer) {
-        button.dataset.answer = options.answer
-    }
-    button.addEventListener('click', checkAnswer)
-    answerEl.appendChild(button);
-    })
-
-};
-
-// check whether option clicked is correct and if there is time left go to next question else end quiz
-function checkAnswer () {
-    // create right/wrong indicaton (apply css here)
-    // pull from questions array and match answer
-    // check if text content matches answer content else false
-    // if false and all answers finished call endQuiz function
-    // if option is false subtract 10 seconds from time
-};
-
-function endQuiz () {
-    // setting the title to "All done!"
-    // time on the clock and make equal to score (final score)
-};
-
-function logScore () {
-    // new element perhaps for text box
-    // clear screen and add in all elements for high score
-    // link to 'View Highscores' on click
-    // log answers to storage
-};
-
 // object array for questions, multiple choice options, and corresponding answers
 var questions = [
     
@@ -91,4 +29,80 @@ var questions = [
     options: ["Javascript", "terminal/bash", "for loops", "console.log"], 
     answer: "console.log"}
 ]
+
+
+var startButton = $('.start-btn');
+$(startButton).on('click', startQuestion);
+
+// answer key format (quiz.method)
+// var quiz = {
+
+// when start button is clicked
+function startQuestion () {
+    // hide all elements in instr card
+    $(card).addClass('hide');
+    // start the clock
+    // pull the questions in and make display (might be easier to make sample question hidden on html)
+    $(parentQuestionEl).removeClass('hide');
+    // shuffle questions
+    shuffledQuestions = questions.sort(()=> Math.random() - .5);
+    currentQuestionIndex = 0;
+    setNextQuestion()
+    // start populating board (questions and answers)
+    // append to card-body div
+};
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+function showQuestion(question) {
+    $(questionEl).text(question.question);
+    answerEl.empty();
+    question.options.forEach(options => {
+        var button = $('<button>') 
+        button.text(options)
+        console.log(options)
+        button.addClass('btn')
+    // if (options.answer) {
+    //     button.dataset.answer = options.answer
+    // }
+    // button.on('click', checkAnswer)
+    answerEl.append(button, '<br/>');
+    })
+    $('.btn').on('click', checkAnswer);
+};
+
+// check whether option clicked is correct and if there is time left go to next question else end quiz
+function checkAnswer () {
+    var textBtn = $(this).text();
+
+    if (textBtn === shuffledQuestions[currentQuestionIndex].answer) {
+        alert('correct')
+    } else {
+        alert('incorrect')
+    };
+    currentQuestionIndex++
+    setNextQuestion();
+    // if alert($(this).text()) === questions. 
+    // create right/wrong indicaton (apply css here)
+    // pull from questions array and match answer
+    // check if text content matches answer content else false
+    // if false and all answers finished call endQuiz function
+    // if option is false subtract 10 seconds from time
+};
+
+function endQuiz () {
+    // setting the title to "All done!"
+    // time on the clock and make equal to score (final score)
+};
+
+function logScore () {
+    // new element perhaps for text box
+    // clear screen and add in all elements for high score
+    // link to 'View Highscores' on click
+    // log answers to storage
+};
+
+
 // };
